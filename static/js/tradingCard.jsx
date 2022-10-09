4f3af2138468e1d2eff53147f45f8597630f02a6
Lauren Glynn <lauren.glynn@gmail.com>
2022-10-08 20:31:19 -0400

    added code to static trading card jsx to include additional trade cards

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,34 +1,83 @@
 'use strict';
 
+
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
   },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Seedpy",
+    skill: "baking",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Merge",
+    skill: "Making Bread",
+    imgUrl: "/static/img/merge.png",
+    cardId: 5,
+  },
+
 ];
+
+// const tradingCardData = [
+//   {
+//     name: 'Balloonicorn',
+//     skill: 'video games',
+//     imgUrl: '/static/img/balloonicorn.jpg',
+//   },
+// ];
+
 
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
 
-ReactDOM.render(
-  <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
-  document.querySelector('#balloonicorn'),
-);
+// function TradingCardContainer() {
+//   return (
+//     <div>
+//       success
+//     </div> 
+//   );
+//  }
 
-ReactDOM.render(
-  <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
-  document.querySelector('#float'),
-);
+// ReactDOM.render(<TradingCardContainer/>, document.querySelector('#all-cards'))
 
-ReactDOM.render(
-  <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
-  document.querySelector('#llambda'),
-);
+function TradingCardContainer() {
+  const tradingCards = [];
+  
+  for (const currentCard of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl={currentCard.imgUrl}
+      />
+    );
+  }
+  
+return (
+  tradingCards
+  );
+}
